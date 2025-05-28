8887b50c33b5699587d41b1f72def8e77eb45338
enzow <enzowatanabedelima@gmail.com>
2025-05-28 08:34:29 -0300

    Loja - Enzo

--- src/main/java/br/pucpr/crud_java/Loja.java
+++ src/main/java/br/pucpr/crud_java/Loja.java
@@ -1,6 +1,68 @@
 package br.pucpr.crud_java;
 
 public class Loja {
+    private int lojaId;
+    private String lojaNome;
+    private String lojaTelefone;
+    private String lojaLogo;
+    private String lojaAndar;
+    private String lojaTipo;
 
 
+    public Loja(int lojaId, String lojaNome, String lojaTelefone, String lojaLogo, String lojaAndar, String lojaTipo) {
+        this.lojaId = lojaId;
+        this.lojaNome = lojaNome;
+        this.lojaTelefone = lojaTelefone;
+        this.lojaLogo = lojaLogo;
+        this.lojaAndar = lojaAndar;
+        this.lojaTipo = lojaTipo;
+    }
+
+    public int getLojaId() {
+        return lojaId;
+    }
+
+    public void setLojaId(int lojaId) {
+        this.lojaId = lojaId;
+    }
+
+    public String getLojaNome() {
+        return lojaNome;
+    }
+
+    public void setLojaNome(String lojaNome) {
+        this.lojaNome = lojaNome;
+    }
+
+    public String getLojaTelefone() {
+        return lojaTelefone;
+    }
+
+    public void setLojaTelefone(String lojaTelefone) {
+        this.lojaTelefone = lojaTelefone;
+    }
+
+    public String getLojaLogo() {
+        return lojaLogo;
+    }
+
+    public void setLojaLogo(String lojaLogo) {
+        this.lojaLogo = lojaLogo;
+    }
+
+    public String getLojaAndar() {
+        return lojaAndar;
+    }
+
+    public void setLojaAndar(String lojaAndar) {
+        this.lojaAndar = lojaAndar;
+    }
+
+    public String getLojaTipo() {
+        return lojaTipo;
+    }
+
+    public void setLojaTipo(String lojaTipo) {
+        this.lojaTipo = lojaTipo;
+    }
 }
