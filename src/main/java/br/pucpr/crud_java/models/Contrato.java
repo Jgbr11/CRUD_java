c3ae27424465adc9e4af7d1733fb99ebd1629a0c
João guilherme cordeiro <jgcreplive@gmail.com>
2025-05-28 11:36:27 -0300

    iniciando Contrato

--- src/main/java/br/pucpr/crud_java/models/Contrato.java
+++ src/main/java/br/pucpr/crud_java/models/Contrato.java
@@ -1,5 +1,23 @@
 package br.pucpr.crud_java.models;
 
 public class Contrato {
+    private int idContrato;
+    private int empresaid;
+    private int espacoid;
+    private String nomeLocatario;
+    private String dataInicio;
+    private double valorMensal;
+    private boolean ContratoStatus;
+
+    public Contrato(int idContrato, int empresaid, int  espacoid, String nomeLocatario, String dataInicio, double valorMensal, boolean ContratoStatus) {
+        this.idContrato = idContrato;
+        this.empresaid = empresaid;
+        this.espacoid = espacoid;
+        this.nomeLocatario = nomeLocatario;
+        this.dataInicio = dataInicio;
+        this.valorMensal = valorMensal;
+        this.ContratoStatus = ContratoStatus;
+    }
+
 
 }
